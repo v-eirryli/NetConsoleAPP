198289092dbc2d51ba3a8f8aad77db30c4682743
Eirry Li <v-eirryli@microsoft.com>
2021-04-12 02:45:14 -0700

    Add project files.

---
+++ NetConsoleAPP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetConsoleAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
